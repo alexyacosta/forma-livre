fab5cd7bfb8b6ee72640726b67e7e671c9195cb3
Alexya de Sousa Costa <alexyacosta@live.com>
2022-11-17 21:19:54 -0300

    Adiciona arquivo com variáveis de ambiente e configura o backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 const entityUsers = require('./entity/users')
 const entityProjects = require('./entity/projects')
 
 const app = express()
+const port = parseInt(process.env.APP_BACKEND_PORT)
 
 // middlewares
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(morgan('common'))
 
 // rotas
 app.use('/api/users', entityUsers)
 app.use('/api/projects', entityProjects)
 
 // entrypoint
-app.listen(9999, () => console.log('listening...'))
+app.listen(port, () => console.log(`listening on port ${port}`))
